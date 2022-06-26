01e7804d785d1ce8625b29c9adb61631433839d4
Mandress101 <67111984+Mandress101@users.noreply.github.com>
2022-06-26 16:45:45 +0200

    Delete SpringCore-API directory

--- SpringCore-API/src/main/java/dev/springcraft/springcore/api/protocol/CallableEvent.java
+++
@@ -1,8 +0,0 @@
-package dev.springcraft.springcore.api.protocol;
-
-import dev.springcraft.springcore.api.protocol.packet.WrappedPacket;
-
-public interface CallableEvent {
-
-    void call(WrappedPacket packet);
-}
